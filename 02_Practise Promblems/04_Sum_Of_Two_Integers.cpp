bf70bfab5aa6352fc31d49e88f510fda096a8c89
ZainNaeem-Official <zainnaeemOfficial@gmail.com>
2025-10-20 09:08:25 +0500

    Sum of Two Integers

---
+++ 02_Practise Promblems/04_Sum_Of_Two_Integers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    
+    cin >> a >> b;           // Read two numbers
+    int sum = a + b;         // Calculate sum
+    cout << sum << endl;     // Print result
+    
+    return 0;
+}
