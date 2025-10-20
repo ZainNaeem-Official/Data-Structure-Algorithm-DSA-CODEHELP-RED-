e64aa305aa6702a8fcf61b920f0d4d2fed2f7727
ZainNaeem-Official <zainnaeemOfficial@gmail.com>
2025-10-20 09:05:52 +0500

    Print Half of an Integerr

---
+++ 02_Practise Promblems/03_PrintHalf_of_an_Integer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    
+    cin >> num;              // Read number
+    int half = num / 2;      // Calculate half
+    cout << half << endl;    // Print result
+    
+    return 0;
+}
